5449fd6814fc201047d518716d9cbfeb2de30321
Monica Ulloa <48863223+monicaud@users.noreply.github.com>
2021-08-24 15:54:05 -0400

    Log records

---
+++ Phase3/log-records/main.js
@@ -0,0 +1,40 @@
+
+var users = [];
+
+readInfo();
+start();
+
+readInfo();
+function readInfo(){
+    let fs = require("fs");
+    if(fs.existsSync("users.json")){
+        users = JSON.parse(fs.readFileSync("users.json").toString());
+    }
+    
+
+   
+}
+
+function start(){
+    let again = true;
+
+    while(again){
+        let readline = require("readline-sync");
+        let fs = require("fs");
+        let fnameUser = readline.question("Enter your first name: ");
+        let lnameUser = readline.question("Enter your last name: ");
+        let genderUser = readline.question("Enter your gender: ");
+        let emailUser = readline.questionEMail("Enter your email: ");
+        let dateUser = new Date();
+        
+        let user = {fname: fnameUser, lname: lnameUser, gender: genderUser, email: emailUser, date: dateUser};
+        users.push(user);
+        let userStr = JSON.stringify(users);
+        fs.writeFileSync("users.json", userStr);
+        let tryAgain = readline.question("Go again? (y/n): ");
+        if(tryAgain == "n" || tryAgain == "N" || tryAgain == "no"){
+            again = false;
+        }
+    }
+    
+}
